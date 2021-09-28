43df209390c109ce8c7f329d1ebc4e81f0ede7a5
matejbb <87907823+matejbb@users.noreply.github.com>
2021-09-28 09:30:01 +0200

    Add files via upload

--- kvadraticka-rovnica.py
+++ kvadraticka-rovnica.py
@@ -2,4 +2,4 @@ a = int(input("Zadaj premennú a: "))
 b = int(input("Zadaj premennú b: "))
 c = int(input("Zadaj premennú c: "))
 d = b**2-4*a*c
-print(((-b+d**(1/2))/2),(((-b-d**(1/2))/2)))
+print(((-b+d**(1/2))/(2*a)),(((-b-d**(1/2))/(2*a))))
